456a21d88d37c87e979df2118764e9c27814135e
Haden Fletcher <33473181+hadenpf@users.noreply.github.com>
2021-10-03 03:40:05 -0500

    fix headline text splitting + add custom tag/component

--- src/components/atoms/Headline.tsx
+++ src/components/atoms/Headline.tsx
@@ -1,14 +1,17 @@
 import * as React from 'react'
-import styles from './Headline.module.scss'
 import classNames from 'classnames'
+
+import AccessibleOnly from '$utils/AccessibleOnly'
+
+import styles from './Headline.module.scss'
 
 export interface HeadlineProps {
 	title: string
 	subtitle?: string
-	punctuate?: null | 'title' | 'subtitle' | 'both'
 
 	titleLink?: string
 
+	component?: React.ComponentType
 	small?: boolean
 	highlight?: boolean
 	noSpacing?: boolean
@@ -18,16 +21,16 @@ const Headline: React.FC<HeadlineProps> = (props) => {
 	const {
 		title,
 		subtitle,
-		punctuate,
 		titleLink,
 		highlight = false,
 		noSpacing = false,
 		small = false,
+		component: Component = 'h1',
 		...rest
 	} = props
 
 	return (
-		<h1
+		<Component
 			className={classNames(styles.Headline, {
 				[styles.noSpacing]: noSpacing,
 				[styles.highlight]: highlight,
@@ -35,25 +38,22 @@ const Headline: React.FC<HeadlineProps> = (props) => {
 			})}
 			{...rest}
 		>
-			{!!titleLink ? (
-				<a className={styles.title} href={titleLink}>
-					{title}
-					{['title', 'both'].includes(punctuate) && '.'}{' '}
-				</a>
-			) : (
+			<span className={styles.innerContent} role="text">
 				<span className={styles.title}>
-					{title}
-					{['title', 'both'].includes(punctuate) && '.'}{' '}
+					<AccessibleOnly>{title}.</AccessibleOnly>
+					<span aria-hidden="true">
+						{!!titleLink ? (
+							<a href={titleLink} className={styles.link}>
+								{title}
+							</a>
+						) : (
+							title
+						)}
+					</span>
 				</span>
-			)}
-			{subtitle && (
-				<span className={styles.subtitle}>
-					{' '}
-					{subtitle}
-					{['subtitle', 'both'].includes(punctuate) && '.'}
-				</span>
-			)}
-		</h1>
+				{subtitle && <span className={styles.subtitle}> {subtitle}</span>}
+			</span>
+		</Component>
 	)
 }
 
